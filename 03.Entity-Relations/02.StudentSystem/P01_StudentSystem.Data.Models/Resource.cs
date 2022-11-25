935f66319776c345c232317ad90ba1ef1bf0f608
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-25 11:19:59 +0200

    Add files via upload

---
+++ 03.Entity-Relations/02.StudentSystem/P01_StudentSystem.Data.Models/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public Enums.Enumerations ResourceType  { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}
---
+++ 03.Entity-Relations/02.StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public StudentSystemContext()
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Resource> Resources { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StudentCourse>(e =>
+            {
+                e.HasKey(k => new { k.CourseId, k.StudentId });
+            });
+
+            
+            modelBuilder.Entity<Student>().Property(e => e.PhoneNumber).HasMaxLength(10).IsFixedLength();
+            
+            modelBuilder.Entity<Student>().Property(e => e.PhoneNumber).IsUnicode(false);
+
+            modelBuilder.Entity<Resource>().Property(e => e.Url).IsUnicode(false);
+
+            modelBuilder.Entity<Homework>().Property(e => e.Content).IsUnicode(false);
+        }
+    }
+}
