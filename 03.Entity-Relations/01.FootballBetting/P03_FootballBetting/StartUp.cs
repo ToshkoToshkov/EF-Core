368de6ef428886a5ec2e60a0ff77265a9178a56c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-25 11:12:59 +0200

    Add files via upload

---
+++ 03.Entity-Relations/01.FootballBetting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FootballBettingContext dbContext = new FootballBettingContext();
+
+            //dbContext.Database.EnsureCreated();
+
+            //Console.WriteLine("Db created succesfully");
+            //Console.WriteLine("Do you want to delete database (Y/N)");
+
+            //string result = Console.ReadLine();
+            // dbContext.Database.EnsureDeleted();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
