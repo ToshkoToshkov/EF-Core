9492674b717ba7e820b8e9aebe7f34bab8343c80
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-26 16:58:29 +0200

    Add files via upload

---
+++ 09.Exam-Preparation-Projects/01.TeisterMask/TeisterMask/Common/Validations.cs
@@ -0,0 +1,27 @@
+﻿namespace TeisterMask.Common
+{
+    public class Validations
+    {
+        //Employees
+        public const int USERNAME_MIN_LENGTH = 3;
+        public const int VALIDATE_USERNAME_LENGTH = 40;
+        public const int VALIDATE_PHONE_LENGTH = 12;
+        public const string USERNAME_REGEX = @"^[A-Za-z0-9]+$";
+        public const string EMPLOYEE_PHONE_REGEX = @"^\d{3}\-\d{3}\-\d{4}$";
+
+        //Projects
+        public const int VALIDATE_NAME_LENGTH = 40;
+        public const int VALIDATE_NAME_MIN_LENGTH = 2;
+
+        //Tasks
+        public const int VALIDATE_NAME_TASK_LENGTH = 40;
+        public const int VALIDATE_NAME_TASK_MIN_LENGTH = 2;
+
+        public const int VALIDATE_EXEC_TYPE_MIN_VALUE = 0;
+        public const int VALIDATE_EXEC_TYPE_MAX_VALUE = 3;
+
+        public const int VALIDATE_LABEL_TYPE_MIN_VALUE = 0;
+        public const int VALIDATE_LABEL_TYPE_MAX_VALUE = 4;
+
+    }
+}
---
+++ 09.Exam-Preparation-Projects/01.TeisterMask/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(Common.Validations.VALIDATE_NAME_LENGTH)]
+        public string Name { get; set; }
+
+        public DateTime OpenDate { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+
+    }
+}
